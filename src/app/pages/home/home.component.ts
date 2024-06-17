396d9cec664decabd6f2841bf4e1ba0f47602f17
lucas <lucas.escaffre@ynov.com>
2024-06-17 16:12:41 +0200

    push: main page with stats, review  etc

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Select } from '@ngxs/store';
 import { UserState } from '../../store/UserState';
 import { Observable } from 'rxjs';
@@ -10,8 +10,35 @@ import { User } from '../../core/models/User';
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   @Select(UserState.selectUser) private user$!: Observable<User>;
   user = toSignal(this.user$);
 
+  stats: { title: string, value: number }[] = [];
+  graphData1: any = {};
+  graphData2: any = {};
+  reviews: { text: string, user: string }[] = [];
+  faqs: { question: string, answer: string }[] = [];
+
+  ngOnInit() {
+    this.stats = [
+      { title: 'Serveurs', value: 1234 },
+      { title: 'Traductions', value: 56789 },
+      { title: 'Langues supportées', value: 34 }
+    ];
+
+    this.reviews = [
+      { text: 'Super bot de traduction, très précis et rapide !', user: 'Emanuel Macaron' },
+      { text: 'Les traductions sont vraiment bonnes. Je l\'utilise tous les jours.', user: 'Alexandre Dessouter' }
+    ];
+
+    this.faqs = [
+      { question: 'Comment inviter le bot sur mon serveur ?', answer: 'Utilisez le bouton "Inviter le Bot" en haut de la page.' },
+      { question: 'Quelles langues sont supportées ?', answer: 'Le bot supporte plus de 30 langues, dont l\'anglais, le français, l\'espagnol, etc.' }
+    ];
+  }
+
+  leaveReview() {
+    alert('Fonctionnalité à venir : laisser un avis.');
+  }
 }
